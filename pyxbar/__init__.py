bc7c2abd9cbb4aed74a51eade8d108dcd1679c94
Adam Hitchcock <adam@northisup.com>
2024-01-12 10:02:49 -0800

    version move

--- pyxbar/__init__.py
+++ pyxbar/__init__.py
@@ -1,4 +1,3 @@
-from .__version__ import __version__ as __version__
 from .config import Config as Config
 from .config import get_config as get_config
 from .menu import Menu as Menu
@@ -20,5 +19,7 @@ from .utils import cache_file as cache_file
 from .utils import camel_to_snake as camel_to_snake
 from .utils import get_in as get_in
 from .utils import strify as strify
+
+__version__ = "0.3.17"
 
 __all__ = ("__version__",)
--- pyxbar/__version__.py
+++
@@ -1,4 +0,0 @@
-__version__ = "0.3.17"
-
-if __name__ == "__main__":
-    print(__version__)
