37cc5014334bd7c283ce9e9b04719a4dfff83885
Adam Hitchcock <adam@northisup.com>
2024-01-11 16:21:38 -0800

    icon helpers

--- pyxbar/__init__.py
+++ pyxbar/__init__.py
@@ -10,12 +10,12 @@ from .menu_items import MonoItem as MonoItem
 from .menu_items import ShellItem as ShellItem
 from .types import Renderable as Renderable
 from .types import RenderableGenerator as RenderableGenerator
+from .utils import cache_dir as cache_dir
+from .utils import cache_file as cache_file
 from .utils import camel_to_snake as camel_to_snake
 from .utils import get_in as get_in
 from .utils import strify as strify
-from .utils import cache_file as cache_file
-from .utils import cache_dir as cache_dir
 
-__version__ = "0.3.6"
+__version__ = "0.3.7"
 
 __all__ = ("__version__",)
